acb0e1f0a373c30f3c41fe7883545801da4cd6b8
Sang_Hyeok <61928927+tkdgur7234@users.noreply.github.com>
2025-01-02 16:32:07 +0900

    (01/02) SearchBox

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.getElementById("generate-btn").addEventListener("click", function () {
-    document.getElementById("quiz-question").textContent = "New Quiz Question: What is 2 + 2?";
-});
+const f = document.getElementById('search');
+const q = document.getElementById('query');
+const google = 'https://www.google.com/search?q=site%3A+';
+
+function submitted(event) {
+  event.preventDefault();
+  const url = google + q.value;
+  const win = window.open(url, '_blank');
+  win.focus();
+}
+
+f.addEventListener('submit', submitted);
